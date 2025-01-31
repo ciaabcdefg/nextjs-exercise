ddbfb488b6ce7faa78661129e7760ef695adeccb
ciaabcdefg <ciaabcdefg@gmail.com>
2025-01-31 11:33:05 +0700

    init

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,8 +21,6 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
-
     return data;
   } catch (error) {
     console.error('Database Error:', error);
@@ -43,6 +41,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
